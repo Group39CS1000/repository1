c04f6b0712071639291d9d7aa12920d0d2bb3382
IP-04 <isaiasxl21@gmail.com>
2023-11-19 18:50:51 -0600

    Isaias Page

---
+++ src/pages/Isaias.js
@@ -0,0 +1,124 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hey, I'm Isaias. </h1>
+          </div>
+          <img
+            className='pic'
+            src='https://i.postimg.cc/zvsvbpnd/Senior-Picture-Isaias-Perez.jpg'
+            alt='Isaias Perez'
+          />
+
+          <p className='description'>
+            Hi, I'm Isaias Perez, a freshman at CU Boulder majoring in Computer Science. 
+            Originally from Caracas, Venezuela, I've been calling Colorado home for the past five years. 
+            My days are often filled with coding and learning new things in my tech classes, but I always make time for my other passions. 
+            I'm really into fitness and spend quite a bit of time working out, which helps me stay energized and focused. 
+            Sports, especially soccer and basketball, are big for me too. Whether it's playing a pickup game or catching a match on TV, I love the excitement and competition. 
+            Balancing my studies with these hobbies keeps me grounded and gives me a great way to unwind. 
+            As I navigate through my first year of college, I'm looking forward to learning as much as I can, both in and out of the classroom.
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
